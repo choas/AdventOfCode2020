d97a1569d764938644927fbd39f089f52851b39a
Lars Gregori <github@larsgregori.de>
2020-12-03 21:18:36 +0100

    use different URL method
    fix "Interrupted system call"
    stop debugger in the readAoC method when an error occurs, otherwise the returned nil will cause later an error
    use ~/AoC2020

--- AdventOfCode2020/util.swift
+++ AdventOfCode2020/util.swift
@@ -9,20 +9,11 @@ import Foundation
 
 func readAoC(file fileName: String) -> String? {
     
-    let documentsUrl = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)[0] as NSURL
+    let homeDirectoryUrl = FileManager.default.homeDirectoryForCurrentUser
     
-    guard let fileUrl = documentsUrl.appendingPathComponent(fileName) else {
-        print("ERROR \(fileName)")
-        return nil
-    }
+    let fileUrl = homeDirectoryUrl
+        .appendingPathComponent("AoC2020")
+        .appendingPathComponent(fileName)
     
-    do {
-        return try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
-        
-    } catch {
-        print("ERROR read file \(fileUrl)")
-        print(error)
-        return nil
-    }
+    return try! String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
 }
-
