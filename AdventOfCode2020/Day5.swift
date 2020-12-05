7ee43de3d3b824500c1ea91ce0075cb441afc794
Lars Gregori <github@larsgregori.de>
2020-12-05 13:09:04 +0100

    move common code for getSeats in own function
    calculate maximum seats based on seatId length (instead of magic number 1024)
    make for-loop in part2 nice

--- AdventOfCode2020/Day5.swift
+++ AdventOfCode2020/Day5.swift
@@ -9,32 +9,24 @@ import Foundation
 
 func aocDay5Part1(fileName: String) -> Int {
     
-    let lines = readAoC(file: fileName)!.components(separatedBy: "\n")
+    let seats = getSeats(fileName)
     
     var maxSeatId = 0
-    lines.forEach{seatId in
-        let seatIdNumber = seatIdToNumber(seatId: seatId)
-        maxSeatId = max(maxSeatId, seatIdNumber)
+    for id in 0..<seats.count {
+        if seats[id] {
+            maxSeatId = max(maxSeatId, id)
+        }
     }
     return maxSeatId
 }
 
 func aocDay5Part2(fileName: String) -> Int {
     
-    let lines = readAoC(file: fileName)!.components(separatedBy: "\n")
-    
-    let maxSeatId = aocDay5Part1(fileName: fileName)
-    
-    var seats = [Bool](repeating: false, count: maxSeatId + 1)
-        lines.forEach{seatId in
-        let seatIdNumber = seatIdToNumber(seatId: seatId)
-        seats[seatIdNumber] = true
-    }
+    let seats = getSeats(fileName)
     
     var missingSeat = -1
-    for id in 1...seats.count - 2 {
+    for id in 1..<seats.count - 1 {
         if !seats[id] && seats[id - 1] && seats[id + 1] {
-            print(id)
             missingSeat = id
         }
     }
@@ -44,11 +36,28 @@ func aocDay5Part2(fileName: String) -> Int {
 
 
 func seatIdToNumber(seatId: String) -> Int {
-    let b = seatId
+    let binaryString = seatId
         .replacingOccurrences(of: "F", with: "0")
         .replacingOccurrences(of: "B", with: "1")
         .replacingOccurrences(of: "L", with: "0")
         .replacingOccurrences(of: "R", with: "1")
     
-    return Int(b, radix: 2)!
+    return Int(binaryString, radix: 2)!
+}
+
+func getSeats(_ fileName: String) -> [Bool] {
+    let lines = readAoC(file: fileName)!.components(separatedBy: "\n")
+    
+    let maxSeats = calcMaxSeats(countChars: lines[0].count)
+    var seats = [Bool](repeating: false, count: maxSeats )
+    lines.forEach{seatId in
+        let seatIdNumber = seatIdToNumber(seatId: seatId)
+        seats[seatIdNumber] = true
+    }
+
+    return seats
+}
+
+func calcMaxSeats(countChars: Int) -> Int {
+    return Int(pow(Double(2), Double(countChars)))
 }
