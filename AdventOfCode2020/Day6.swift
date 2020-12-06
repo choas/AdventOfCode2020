faf5e0d34c2d33dc2e926ba2c5f009ccaac88d1d
Lars Gregori <138015+choas@users.noreply.github.com>
2020-12-06 13:18:56 +0100

    refactor day 6
    collecting the answers in own function for both parts
    return an array with persons and answers

--- AdventOfCode2020/Day6.swift
+++ AdventOfCode2020/Day6.swift
@@ -11,49 +11,57 @@ func aocDay6Part1(fileName: String) -> Int {
     let lines = readAoC(file: fileName)!.components(separatedBy: "\n")
 
     var count = 0
-    var answers: [String: Bool] = [:]
+    let answers = getAnswers(lines)
 
-    lines.forEach { line in
-        if line.count == 0 {
-            count = answers.reduce(count) {counter, answer in
-                return answer.value ? counter + 1 : counter
-            }
-            answers = [:]
-        } else {
-            line.forEach { char in
-                answers[String(char)] = true
-            }
+    answers.forEach { answerGroup in
+        count = answerGroup.answers.reduce(count) {counter, answer in
+            return answer.value > 0 ? counter + 1 : counter
         }
     }
-    return answers.reduce(count) {counter, answer in
-        return answer.value ? counter + 1 : counter
-    }
+
+    return count
 }
 
 func aocDay6Part2(fileName: String) -> Int {
     let lines = readAoC(file: fileName)!.components(separatedBy: "\n")
 
     var count = 0
-    var answers: [String: Int] = [:]
+    let answers = getAnswers(lines)
 
+    answers.forEach { answerPerGroup in
+        count = answerPerGroup.answers.reduce(count) {counter, answer in
+            return answer.value == answerPerGroup.persons ? counter + 1 : counter
+        }
+    }
+
+    return count
+}
+
+struct GroupAnswer {
+    var persons: Int
+    var answers: [String: Int]
+}
+
+private func getAnswers(_ lines: [String]) -> [GroupAnswer] {
+
+    var answers: [GroupAnswer] = []
+    var answersPerGroup: [String: Int] = [:]
     var personCount = 0
-    lines.forEach { line in
 
+    lines.forEach { line in
         if line.count == 0 {
-            count = answers.reduce(count) {counter, answer in
-                return answer.value == personCount ? counter + 1 : counter
-            }
-            answers = [:]
+            answers.append(GroupAnswer(persons: personCount, answers: answersPerGroup))
+            answersPerGroup = [:]
             personCount = 0
         } else {
             personCount += 1
             line.forEach { char in
-                let answer = answers[String(char)]
-                answers[String(char)] = answer != nil ? answer! + 1 : 1
+                let answer = answersPerGroup[String(char)]
+                answersPerGroup[String(char)] = answer != nil ? answer! + 1 : 1
             }
         }
     }
-    return answers.reduce(count) {counter, answer in
-        return answer.value == personCount ? counter + 1 : counter
-    }
+    answers.append(GroupAnswer(persons: personCount, answers: answersPerGroup))
+
+    return answers
 }
